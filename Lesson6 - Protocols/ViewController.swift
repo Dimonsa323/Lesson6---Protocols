dd272d98a47b31c53e83fdca9d396747be17221e
Dima Hubenya <dima96gubenya@gmail.com>
2022-07-17 17:45:52 +0300

    Initial Commit

---
+++ Lesson6 - Protocols/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson6 - Protocols
+//
+//  Created by Дима Губеня on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
